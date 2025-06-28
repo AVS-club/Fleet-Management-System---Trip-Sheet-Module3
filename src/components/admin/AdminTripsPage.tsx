d8528d129eaa89e381f45a2edcde001c6af849ee
slogan79-79 <slogan_79_asp@icloud.com>
2025-06-28 13:25:17 +0530

    Updated AdminTripsPage.tsx

--- src/components/admin/AdminTripsPage.tsx
+++ src/components/admin/AdminTripsPage.tsx
@@ -38,6 +38,10 @@
            </div>
  
            {showFilters && (
+               )
+               }
+       )
+       }
 -            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
 +            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
                {/* Search */}
