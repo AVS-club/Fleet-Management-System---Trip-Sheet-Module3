fa2e13e4318ca077a4fd301f65e446e4488e1f7a
buglertaxi7f <bugler.taxi7f@icloud.com>
2025-07-08 14:58:30 +0530

    Fix import error in DriverInsightsPage

--- src/pages/drivers/DriverInsightsPage.tsx
+++ src/pages/drivers/DriverInsightsPage.tsx
@@ -45,7 +45,7 @@ import Input from "../../components/ui/Input";
 import Select from "../../components/ui/Select";
 import StatCard from "../../components/ui/StatCard";
 import { cn } from "../../utils/cn";
-import { getDrivers, getTrips, getVehicles } from "../../utils/supabaseClient";
+import { getDrivers, getTrips, getVehicles } from "../../utils/storage";
 import type { Driver, Trip, Vehicle } from "../../types";
 
 // Interface for driver performance metrics
