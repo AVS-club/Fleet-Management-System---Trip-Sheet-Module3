6f57d7f2b80710b9020bc2a585bfe553119981ed
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2025-07-12 15:07:01 +0530

    readded currency input component

--- src/components/ui/CurrencyInput.tsx
+++ src/components/ui/CurrencyInput.tsx
@@ -1,4 +1,27 @@
+import React, { forwardRef } from "react";
+import Input from "./Input";
 
+interface CurrencyInputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {
+  label?: string;
+  helperText?: string;
+  error?: string;
+  fullWidth?: boolean;
+  iconPosition?: "left" | "right";
+  size?: "sm" | "md" | "lg";
+}
+
+const CurrencyInput = forwardRef<HTMLInputElement, CurrencyInputProps>(
+  ({ iconPosition = "left", ...props }, ref) => (
+    <Input
+      {...props}
+      ref={ref}
+      type="number"
+      step="0.01"
+      icon={<span>₹</span>}
+      iconPosition={iconPosition}
+    />
+  )
 );
 
 export default CurrencyInput;
