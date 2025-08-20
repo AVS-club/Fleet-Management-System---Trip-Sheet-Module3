74632138a3384db11ad343c2955659f1dbc3b9ce
AVS-club <autovitalsolution@gmail.com>
2025-08-20 16:20:26 +0530

    Update storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1113,7 +1113,6 @@ export const createDriver = async (
           !isNaN(new Date(driverData.license_expiry_date).getTime()) &&
           new Date(driverData.license_expiry_date)) ||
         null,
-      added_by:userId,
     })
     .select()
     .single();
