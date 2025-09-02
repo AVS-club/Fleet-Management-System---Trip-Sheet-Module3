a926913e056f0d8f2900bc9187017bc71fcd4c1f
cortex3f <chubs_cortex_3f@icloud.com>
2025-09-02 09:45:21 +0530

    Fix missing isNetworkError import in storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,8 @@
 import { getCurrentUserId, withOwner } from './supaHelpers';
 import { loadGoogleMaps } from './googleMapsLoader';
 import { handleSupabaseError } from './errors';
+import { supabase, isNetworkError } from './supabaseClient';
+import config from './env';
 import { getVehicle, deleteVehicle } from './api';
 export * from './api';
 
