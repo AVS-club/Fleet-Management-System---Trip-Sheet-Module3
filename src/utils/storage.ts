00f50e0e818d08b1f9db2023b52efa2399672c63
cortex3f <chubs_cortex_3f@icloud.com>
2025-09-02 23:52:04 +0530

    Updated storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,7 +4,9 @@ import { handleSupabaseError } from './errors';
 import { supabase, isNetworkError } from './supabaseClient';
 import config from './env';
 import { getVehicle, deleteVehicle } from './api';
+import { getTrips } from './api/trips';
 export * from './api';
+export { getTrips };
 
 export async function getUserData() {
   try {
